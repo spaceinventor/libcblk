9269a58a3ae9c55b030f50de84d733f00ea41aef
Troels Jessen <troels@space-inventor.com>
2026-03-19 12:55:18 +0100

    Mark crypto key parameters as HWREG to exclude them from typical pulls

--- src/crypto/crypto_param.c
+++ src/crypto/crypto_param.c
@@ -16,9 +16,9 @@ void tx_encrypt_cb(param_t * param, int idx) {
     }
 }
 
-PARAM_DEFINE_STATIC_VMEM(PARAMID_CRYPTO_KEY1,              crypto_key1,             PARAM_TYPE_DATA,   32, sizeof(uint8_t),  PM_READONLY | PM_CRYPT, crypto_key_generate, NULL, crypto, 0x40, "Encryption key 1");
-PARAM_DEFINE_STATIC_VMEM(PARAMID_CRYPTO_KEY2,              crypto_key2,             PARAM_TYPE_DATA,   32, sizeof(uint8_t),  PM_READONLY | PM_CRYPT, crypto_key_generate, NULL, crypto, 0x60, "Encryption key 2");
-PARAM_DEFINE_STATIC_VMEM(PARAMID_CRYPTO_KEY3,              crypto_key3,             PARAM_TYPE_DATA,   32, sizeof(uint8_t),  PM_READONLY | PM_CRYPT, crypto_key_generate, NULL, crypto, 0x80, "Encryption key 3");
+PARAM_DEFINE_STATIC_VMEM(PARAMID_CRYPTO_KEY1,              crypto_key1,             PARAM_TYPE_DATA,   32, sizeof(uint8_t),  PM_HWREG | PM_CRYPT, crypto_key_generate, NULL, crypto, 0x40, "Encryption key 1");
+PARAM_DEFINE_STATIC_VMEM(PARAMID_CRYPTO_KEY2,              crypto_key2,             PARAM_TYPE_DATA,   32, sizeof(uint8_t),  PM_HWREG | PM_CRYPT, crypto_key_generate, NULL, crypto, 0x60, "Encryption key 2");
+PARAM_DEFINE_STATIC_VMEM(PARAMID_CRYPTO_KEY3,              crypto_key3,             PARAM_TYPE_DATA,   32, sizeof(uint8_t),  PM_HWREG | PM_CRYPT, crypto_key_generate, NULL, crypto, 0x80, "Encryption key 3");
 
 PARAM_DEFINE_STATIC_VMEM(PARAMID_TX_ENCRYPT,               tx_encrypt,              PARAM_TYPE_UINT8,  -1, sizeof(uint8_t),  PM_CONF | PM_CRYPT,     tx_encrypt_cb,       NULL, crypto, 0x0A, "Enable encryption using key X (0 disables encryption).");
 PARAM_DEFINE_STATIC_VMEM(PARAMID_RX_ENCRYPT,               rx_decrypt,              PARAM_TYPE_UINT8,  -1, sizeof(uint8_t),  PM_CONF | PM_CRYPT,     NULL,                NULL, crypto, 0x0B, "Expect encryption for X reboots.");
