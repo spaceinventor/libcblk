c0b4c2ec08cad1a00163f0989273d629d75d6a0e
Troels Jessen <troels@space-inventor.com>
2026-04-13 19:29:57 +0200

    Create config header file to define if RS encode is enabled

--- include/cblk/csp_if_cblk.h
+++ include/cblk/csp_if_cblk.h
@@ -5,6 +5,8 @@
 #include <csp/csp.h>
 #include <param/param.h>
 
+#include <libcblk.h>
+
 typedef enum { CBLK_EXTHDR_NOSUPPORT = 0b00, CBLK_EXTHDR_SUPPORT = 0b01, CBLK_EXTHDR_PRESENT = 0b10} ext_hdr_e;
 
 typedef struct __attribute__((packed))
